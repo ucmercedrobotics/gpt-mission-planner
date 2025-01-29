e582be192104665506c083e26b95266d579902ce
Marcos <mzuzuarregui@ucmerced.edu>
2025-01-28 20:57:57 -0800

    refactoring context location

--- app/gpt_interface.py
+++ app/gpt_interface.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from openai.types.chat.chat_completion import ChatCompletion
 
+from context import ifac_2025_context
+
 
 class GPTInterface:
     def __init__(
@@ -30,33 +32,8 @@ class GPTInterface:
             # all robots must come with a schema
             self._set_schema(s)
 
-        # default context
-        self.context: list = [
-            {
-                "role": "user",
-                "content": "You are a mission planner that generates navigational XML mission plans based on robotic task representation. \
-                            When asked to generate a mission, select the appropriate schema to generate an XML mission and \
-                            use the context files to provide references in the mission plan for how the robot tasked with this mission should go about doing it. \
-                            Within the context files you'll find information that should enable you to determine how the mission should operate. \
-                            If not, simply state that the mission is unachieveable and do NOT provide a mission XML. \
-                            Place the original question in the TaskDescription element of the CompositeTaskInformation element for logging.",
-            },
-            {
-                "role": "user",
-                "content": "Your focus is in a precision agriculture setting. \
-                            All of the prompts you will receive should return answers with the domain of precision ag.",
-            },
-            # context
-            {
-                "role": "user",
-                "content": "Here are the list of schemas that represent that available robots you have to accomplish your mission. \
-                            You should remember you can only assign one robot to accomplish the task. \
-                            It is critical that the XML validates against the schema and that the schema location attribute is included in the root tag. \
-                            Please include the XSI schema location every time you generate a mission. \
-                            The mission must be syntactically correct and validate using an XML linter: "
-                + self.schemas,
-            },
-        ]
+        # context can be updated from context.py
+        self.context: list = ifac_2025_context(self.schemas)
 
         # this could be empty
         if context_files is not None:

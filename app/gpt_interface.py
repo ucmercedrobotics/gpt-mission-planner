1c3819c7dd2ac339389a303e7e9268c59c38e62e
alieabd <im.ali.abedi@gmail.com>
2026-04-09 14:22:20 -0700

    refactor YAML adding local model configs

--- app/gpt_interface.py
+++ app/gpt_interface.py
@@ -23,6 +23,7 @@ class LLMInterface:
         max_tokens: int = 2000,
         temperature: float = 0.2,
         context_template: str = "tfr_2026",
+        api_base: str|None = None,
     ):
         self.logger: logging.Logger = logger
         # max number of tokens that GPT will respond with, almost 1:1 with words to token
@@ -38,6 +39,7 @@ class LLMInterface:
         self.promela_template: str = ""
         self.context_template: str = context_template
         self.temperature: float = temperature
+        self.api_base: str | None = api_base
         self.reasoning: str | None = None
         if "openai" in self.model:
             self.logger.warning(
@@ -132,7 +134,7 @@ class LLMInterface:
                     temperature=self.temperature,
                     reasoning_effort=self.reasoning,
                     max_tokens=self.max_tokens,
-                    api_base="http://172.17.0.1:8000/v1"
+                    api_base=self.api_base
                 )
                 answered = True
             except litellm.exceptions.RateLimitError as e:
